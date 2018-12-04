65c70372102a54f841305fe5be59cf5905a9fbce
murat <murat@useinoff.ru>
2018-12-04 18:16:12 +0300

    Start to commit this repo.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # sorts
+
+04.12.2018
+Start to commit this repo. Let's try some bubble-sort at first!
---
+++ bubble-sort.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: murat
+ * Date: 04.12.18
+ * Time: 18:15
+ */
+
+echo 'test';
