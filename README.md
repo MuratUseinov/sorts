be348ef5fcd2905227224c4d619922edbcac31d9
murat <murat@useinoff.ru>
2018-12-04 18:36:50 +0300

    Let's try some bubble-sort at first!

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 04.12.2018
 Start to commit this repo. Let's try some bubble-sort at first!
+##BUBLE SORT
+Asymptotics in worst case – O(n2), in best case – O(n).
--- bubble-sort.php
+++ bubble-sort.php
@@ -6,4 +6,42 @@
  * Time: 18:15
  */
 
-echo 'test';
+//Asymptotics in worst case – O(n2), in best case – O(n).
+class BubbleSort
+{
+    public static function sortWithForeach(&$array)
+    {
+        foreach ($array as $firstKey => $item) {
+            foreach ($array as $secondKey => $nextItem) {
+                if (isset($array[$secondKey + 1])) {
+                    if ($array[$secondKey] > $array[$secondKey + 1]) {
+                        list($array[$secondKey], $array[$secondKey + 1]) = array($array[$secondKey + 1], $array[$secondKey]);
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+
+    public static function sortWithFor(&$array)
+    {
+        $size = count($array) - 1;
+        for ($i = 0; $i < $size; $i++) {
+            for ($j = 0; $j < $size - $i; $j++) {
+                $k = $j + 1;
+                if ($array[$k] < $array[$j]) {
+                    list($array[$j], $array[$k]) = array($array[$k], $array[$j]);
+                }
+            }
+        }
+        return $array;
+    }
+}
+
+$array = [156, 0, 3, 1, 9, 2, 5, 6, 8, 0, 100, 3, 1, 9, 2, 5, 6, 8, 7, 7, 7, 7, 7];
+BubbleSort::sortWithFor($array);
+print_r($array);
+
+$array = [156, 0, 3, 1, 9, 2, 5, 6, 8, 0, 100, 3, 1, 9, 2, 5, 6, 8, 7, 7, 7, 7, 7];
+BubbleSort::sortWithFor($array);
+print_r($array);
